4b59f914ae9d41dea2596987db11fa066225a7d8
aliaboud935 <aliaboud241@gmail.com>
2026-07-11 17:28:11 +0100

    modif

---
+++ exo2.py
@@ -0,0 +1,7 @@
+first_name =input("enter ure first name")
+university = input("enter ure university name")
+year= str(2026)
+message= first_name + university + year
+print("hello my name is ",first_name ,"i graduated from", university,"in the year 2026")
+print(len(message))
+print("graduated"in message)
