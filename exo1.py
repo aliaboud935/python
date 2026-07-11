f74b1b0eeb141d1e3aed1703ff8696bae5115a3b
aliaboud935 <aliaboud241@gmail.com>
2026-07-11 17:04:50 +0100

    oo

---
+++ exo1.py
@@ -0,0 +1,6 @@
+Math = int(input("enter ure math grade"))
+python = int(input("enter ure python grade"))
+database = int(input("enter ure database grade"))
+sum = (Math + python + database )
+average= sum/3
+print(average>10)
